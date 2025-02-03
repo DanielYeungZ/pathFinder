8fb9d3cd4f746dc9b2bd2ecf4c7eee5660f06177
daniel <daniel@famepick.com>
2025-02-02 18:32:07 -0800

    init image

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN_SECRET_KEY = 'pathFinder_user'
+S3_BUCKET='robo-path-image'
+AWS_DEFAULT_REGION="us-east-2"
+# Configure Roboflow
+ROBOFLOW_API_KEY = 'your-roboflow-api-key'
+ROBOFLOW_UPLOAD_URL = 'https://api.roboflow.com/dataset/your-dataset/upload'
+ROBOFLOW_FEATURE=False
