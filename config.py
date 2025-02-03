b857d5945c7a858118f3070cab6fc28260166384
daniel <daniel@famepick.com>
2025-02-02 18:33:40 -0800

    tests

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 TOKEN_SECRET_KEY = 'pathFinder_user'
 S3_BUCKET='robo-path-image'
+S3_KEY=""
+S3_KEY_ID=""
+cc=""
 AWS_DEFAULT_REGION="us-east-2"
 # Configure Roboflow
 ROBOFLOW_API_KEY = 'your-roboflow-api-key'
