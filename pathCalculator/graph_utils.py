bfb990ba732cb7eabee7a29df0704cc83f78dd31
daniel <daniel@famepick.com>
2025-04-09 10:38:32 -0700

    add graph log

--- pathCalculator/graph_utils.py
+++ pathCalculator/graph_utils.py
@@ -1,9 +1,12 @@
 import networkx as nx
 import math
+from services import path_logs
 
 
 def create_graph(binary_image):
+
     graph = nx.Graph()
+    path_logs(f"init graph=====> ")
     rows, cols = binary_image.shape
 
     for row in range(rows):
@@ -13,7 +16,11 @@ def create_graph(binary_image):
                     neighbor_row, neighbor_col = row + dx, col + dy
                     if 0 <= neighbor_row < rows and 0 <= neighbor_col < cols:
                         if binary_image[neighbor_row, neighbor_col] == 255:
-                            graph.add_edge((row, col), (neighbor_row, neighbor_col), weight=1)
+                            path_logs(f"add_edge graph=====> ")
+                            graph.add_edge(
+                                (row, col), (neighbor_row, neighbor_col), weight=1
+                            )
+    path_logs(f"graph=====> {len(graph.nodes())}")
     return graph
 
 
@@ -21,12 +28,14 @@ def shortest_path(graph, start, end):
     def euclidean_distance(a, b):
         return math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
 
-    path = nx.astar_path(graph, source=start, target=end, heuristic=euclidean_distance, weight='weight')
+    path = nx.astar_path(
+        graph, source=start, target=end, heuristic=euclidean_distance, weight="weight"
+    )
     return path
 
 
 # Function to find the shortest path
 def shortest_path1(graph, start, end):
     # Calculate the shortest path using Dijkstra's algorithm
-    path = nx.shortest_path(graph, source=start, target=end, weight='weight')
+    path = nx.shortest_path(graph, source=start, target=end, weight="weight")
     return path
